c853ee5b5fefec3cf4fcbe021f1b25ddf58c7923
root <qiuheng@fit2cloud.com>
2018-11-21 15:53:36 +0800

    new content

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World! Hello Openshift222!</p>'; ?> 
+ <?php echo '<p>Hello World! Hello Openshift333!</p>'; ?> 
  </body>
 </html>
